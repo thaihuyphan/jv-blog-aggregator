a3b37eba6ffaf4722e9ba6d0e521a6b030c4e880
thai-huy <thaihuyphan@gmail.com>
2015-06-16 18:14:59 +0700

    come back again

--- src/main/java/vn/co/huypt/isba/controller/UserController.java
+++ src/main/java/vn/co/huypt/isba/controller/UserController.java
@@ -3,6 +3,7 @@ package vn.co.huypt.isba.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import vn.co.huypt.isba.service.UserService;
@@ -16,7 +17,12 @@ public class UserController {
 	@RequestMapping("/users")
 	public String users(Model model) {
 		model.addAttribute("users", userService.findAll());
-
 		return "users";
+	}
+	
+	@RequestMapping("/users/{id}")
+	public String detail(Model model, @PathVariable int id) {
+		model.addAttribute("user", userService.findOne(id));
+		return "user-detail";
 	}
 }
--- src/main/webapp/WEB-INF/jsp/users.jsp
+++ src/main/webapp/WEB-INF/jsp/users.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring"%>
 
 <table class="table table-bordered table-hover table-striped">
 	<thead>
@@ -11,7 +12,8 @@
 	<tbody>
 		<c:forEach items="${users}" var="user">
 			<tr>
-				<td>${user.name}</td>
+				<td><a href="<spring:url value="/users/${user.id}.html" />">
+						${user.name} </a></td>
 			</tr>
 		</c:forEach>
 	</tbody>
