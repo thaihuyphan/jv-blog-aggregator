3d173e24f17d4c5259fb1755445d5dc43476dae6
thai-huy <thaihuyphan@gmail.com>
2015-07-17 15:11:12 +0700

    sda

--- src/main/java/vn/co/huypt/isba/service/BlogService.java
+++ src/main/java/vn/co/huypt/isba/service/BlogService.java
@@ -1,6 +1,8 @@
 package vn.co.huypt.isba.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.method.P;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import vn.co.huypt.isba.entity.Blog;
@@ -23,7 +25,12 @@ public class BlogService {
 		blogRepository.save(blog);
 	}
 
-	public void delete(int id) {
-		blogRepository.delete(id);
+	@PreAuthorize("#blog.user.name == authentication.name or hasRole('ROLE_ADMIN')")
+	public void delete(@P("blog") Blog blog) {
+		blogRepository.delete(blog);
+	}
+
+	public Blog finOne(int id) {
+		return blogRepository.findOne(id);
 	}
 }
