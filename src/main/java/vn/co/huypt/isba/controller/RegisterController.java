4e6af2752d8e36028d22b6b4dce468d3e69e00e6
thai-huy <thaihuyphan@gmail.com>
2015-07-17 18:03:48 +0700

    maybe final today guy

---
+++ src/main/java/vn/co/huypt/isba/controller/RegisterController.java
@@ -0,0 +1,41 @@
+package vn.co.huypt.isba.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import vn.co.huypt.isba.entity.User;
+import vn.co.huypt.isba.service.UserService;
+
+@Controller
+@RequestMapping("/register")
+public class RegisterController {
+
+	@Autowired
+	private UserService userService;
+	
+	@ModelAttribute("user")
+	public User constructUser() {
+		return new User();
+	}
+
+	@RequestMapping
+	public String showRegister() {
+		return "user-register";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String doRegister(@Valid @ModelAttribute("user") User user,
+			BindingResult result) {
+		if (result.hasErrors()) {
+			return "user-register";
+		}
+		userService.save(user);
+		return "redirect:/register.html?success=true";
+	}
+}
--- src/main/java/vn/co/huypt/isba/controller/UserController.java
+++ src/main/java/vn/co/huypt/isba/controller/UserController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import vn.co.huypt.isba.entity.Blog;
-import vn.co.huypt.isba.entity.User;
 import vn.co.huypt.isba.service.BlogService;
 import vn.co.huypt.isba.service.UserService;
 
@@ -27,48 +26,16 @@ public class UserController {
 	@Autowired
 	private BlogService blogService;
 
-	@ModelAttribute("user")
-	public User constructUser() {
-		return new User();
-	}
-
 	@ModelAttribute("blog")
 	public Blog constructBlog() {
 		return new Blog();
-	}
-
-	@RequestMapping("/users")
-	public String users(Model model) {
-		model.addAttribute("users", userService.findAll());
-		return "users";
-	}
-
-	@RequestMapping("/users/{id}")
-	public String detail(Model model, @PathVariable int id) {
-		model.addAttribute("user", userService.findOneWithBlogs(id));
-		return "user-detail";
-	}
-
-	@RequestMapping("/register")
-	public String showRegister() {
-		return "user-register";
-	}
-
-	@RequestMapping(value = "/register", method = RequestMethod.POST)
-	public String doRegister(@Valid @ModelAttribute("user") User user,
-			BindingResult result) {
-		if (result.hasErrors()) {
-			return "user-register";
-		}
-		userService.save(user);
-		return "redirect:/register.html?success=true";
 	}
 
 	@RequestMapping("/account")
 	public String account(Model model, Principal principal) {
 		String name = principal.getName();
 		model.addAttribute("user", userService.findOneWithBlogs(name));
-		return "user-detail";
+		return "account";
 	}
 
 	@RequestMapping(value = "/account", method = RequestMethod.POST)
@@ -90,9 +57,4 @@ public class UserController {
 		return "redirect:/account.html";
 	}
 
-	@RequestMapping("/users/remove/{id}")
-	public String removeUser(@PathVariable int id) {
-		userService.delete(id);
-		return "redirect:/users.html";
-	}
 }
