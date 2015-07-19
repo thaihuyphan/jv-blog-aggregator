a6d3de73a8f73e404a0799cfca129337831720ad
phant_000 <phant_000@192.168.1.23>
2015-07-19 14:34:28 +0700

    aaa

--- src/main/java/vn/co/huypt/isba/service/ItemService.java
+++ src/main/java/vn/co/huypt/isba/service/ItemService.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
 
 import vn.co.huypt.isba.entity.Item;
 import vn.co.huypt.isba.repository.ItemRepository;
 
+@Service
 public class ItemService {
 
 	@Autowired
